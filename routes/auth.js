75e9d727d33cb043bdca9e56c4820b1ef297dbc6
Sweet Lea Atay <jesterabalos244@gmail.com>
2025-10-28 03:33:02 +0800

    Fix bcrypt error by switching to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
   const express = require("express");
-  const bcrypt = require("bcrypt");
+ const bcrypt = require("bcryptjs");
   const AdminAccount = require("../models/AdminAccount"); // FIXED PATH
   const WebsiteAdmin = require("../models/WebsiteAdmin"); // FIXED PATH
   const { logActivity } = require("../utils/helpers"); // FIXED PATH
